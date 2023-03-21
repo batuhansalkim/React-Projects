b744df8264ce7353c743e7c18b1fbbca84d531c4
Baturpcu <batusalkim11@gmail.com>
2023-03-22 02:41:12 +0300

    update

--- Projects/my-projects/src/App.js
+++ Projects/my-projects/src/App.js
@@ -3,20 +3,22 @@ import './App.css';
 function App() {
   return (
     <div className="hero">
-      <div className='container'>
-        <p>Random Number : <span>5</span></p>
-      </div>
-      <div className='numContainer'>
-        <div>
-          <p>Min:</p>
-          <input type="number" />
+      <div className="container">
+        <div className="randomNum">
+          <p>Random Number: <span>5</span></p>
         </div>
-        <div>
-          <p>Max:</p>
-          <input type="number" />
+        <div className="numContainer">
+          <div>
+            <p>Min:</p>
+            <input type="number"/>
+          </div>
+          <div>
+            <p>Max:</p>
+            <input type="number" />
+          </div>
         </div>
+        <button >Get random number</button>
       </div>
-      <button>Get Random Number</button>
     </div>
   );
 }
