d5e17199b7ba5ccf1418402cacad8a14a76a8aa4
Baturpcu <batusalkim11@gmail.com>
2023-03-22 20:48:40 +0300

    update

--- Projects/my-projects/src/proje3/app3.js
+++ Projects/my-projects/src/proje3/app3.js
@@ -1,9 +1,18 @@
  import "../proje3/app3.css";
+ import "../proje3/index.css";
  const App3 =()=>{
     return(
         <div className="container">
-            <h1>akdhjsahd</h1>
+            <h1>Todo List App</h1>
+            <input type="text" placeholder="add an item..." />
+            <button>Add</button>
+            <ul>
+                <li>react çalış</li>
+                <li>js çalış</li>
+                <li>c# çalış</li>
+            </ul>
         </div>
+        
     )
 }
 export default App3;
