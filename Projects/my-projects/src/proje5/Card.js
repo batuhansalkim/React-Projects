411ddf7b08df0bcc1472c694a60c7273b05bc0d5
Baturpcu <batusalkim11@gmail.com>
2023-03-23 02:49:54 +0300

    update

--- Projects/my-projects/src/proje5/Card.js
+++ Projects/my-projects/src/proje5/Card.js
@@ -1,15 +1,16 @@
 import React from "react";
-import "../proje5/app5.css";
+import "../proje5/card.css";
 
-export default function Card(){
+export default function Card(props){
     return(
         <div className="card">
             <div className="cardBody">
-                <img src="https://media.licdn.com/dms/image/C4D03AQEohqwLUIK0yg/profile-displayphoto-shrink_800_800/0/1605612810263?e=2147483647&v=beta&t=u1JLSkD6Z2GL4ppEI-9B-2iGDiO9vm97utliL9sYt1U" alt="" />
+                <img src={props.img} alt="" />
                 <div className="about">
-                    <h2 className="cardTitle">batuhan</h2>
-                    <p className="cardDescription">Batuhan salkım linkedin foto</p>
+                    <h2 className="cardTitle">{props.title}</h2>
+                    <p className="cardDescription">{props.description}</p>
                 </div>
+                <button>See The Details</button>
             </div>
         </div>
     )
--- Projects/my-projects/src/proje5/app5.js
+++ Projects/my-projects/src/proje5/app5.js
@@ -4,9 +4,22 @@ import Card from "../proje5/Card";
 function Appbatu(){
     return(
         <div className="App5">
-            <Card/>
-            <Card/>
-            <Card/>
+            <Card
+                img="https://media.licdn.com/dms/image/C4D03AQEohqwLUIK0yg/profile-displayphoto-shrink_800_800/0/1605612810263?e=2147483647&v=beta&t=u1JLSkD6Z2GL4ppEI-9B-2iGDiO9vm97utliL9sYt1U"
+            title="Batuhan SALKIM"
+            description="batuhan salkım linkedin profile photo"
+            />
+            <Card
+                img="https://fotolifeakademi.com/uploads/2020/12/cicek-fotografi-cekimi-workshop.webp"
+                title="batuslkm"
+                description="batuhan salkım ınstagram profile photo"
+            />
+            <Card
+                img="https://orcun.baslak.com/wp-content/uploads/2022/02/nft-960x400.jpg"
+                title="nft"
+                description="free nft monkey "
+            />
+            
         </div>
     );
 }
