27f45559295e60c2c2ea24b53dc8faa95830e75f
Baturpcu <batusalkim11@gmail.com>
2023-03-22 22:21:33 +0300

    update

--- Projects/my-projects/src/App.js
+++ Projects/my-projects/src/App.js
@@ -2,26 +2,31 @@ import React,{useState} from 'react';
 import './App.css';
 
 function App() {
-  
+  const [maxVal,setMaxVal] = useState(0);
+  const [minVal,setMinVal] = useState(0);
+  const [randomNumber, setRandomNumber] = useState(5);
 
+  const generateRandomNumber=()=>{
+    setRandomNumber(Math.floor(Math.random()* (maxVal-minVal+1)+minVal))
+  }
   
   return (
     <div className="hero">
       <div className="container">
         <div className="randomNum">
-          <p>Random Number: <span></span></p>
+          <p>Random Number: <span>{randomNumber}</span></p>
         </div>
         <div className="numContainer">
           <div>
-            <p>Min:</p>
-            <input type="number"  />
+            <p>Min: {minVal}</p>
+            <input type="number" onChange={e=>setMinVal(e.target.value)} />
           </div>
           <div>
-            <p>Max:</p>
-            <input type="number" />
+            <p>Max: {maxVal}</p>
+            <input type="number" onChange={e=>setMaxVal(e.target.value)}/>
           </div>
         </div>
-        <button >Get random number</button>
+        <button onClick={generateRandomNumber}>Get random number</button>
       </div>
     </div>
   );
