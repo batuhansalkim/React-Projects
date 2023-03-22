187b95fc93c1ac2de06b3dedf9fe08107c80c619
Baturpcu <batusalkim11@gmail.com>
2023-03-22 03:25:06 +0300

    update

--- Projects/my-projects/src/App.js
+++ Projects/my-projects/src/App.js
@@ -1,23 +1,31 @@
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const [minVal, setMinVal] = useState(0);
+  const [maxVal,setMaxVal] = useState(0);
+  const [randomNum,setRandomNum] = useState(7);
+
+  const giveRandomNumber=()=>{
+    setRandomNum(Math.floor(Math.random()*(maxVal-minVal +1)));
+  }
   return (
     <div className="hero">
       <div className="container">
         <div className="randomNum">
-          <p>Random Number: <span>5</span></p>
+          <p>Random Number: <span>{randomNum}</span></p>
         </div>
         <div className="numContainer">
           <div>
             <p>Min:</p>
-            <input type="number"/>
+            <input type="number" value={minVal} onChange={e=>setMinVal(e.target.value)}/>
           </div>
           <div>
             <p>Max:</p>
-            <input type="number" />
+            <input type="number" value={maxVal} onChange={e=>setMaxVal(e.target.value)}/>
           </div>
         </div>
-        <button >Get random number</button>
+        <button onClick={giveRandomNumber}>Get random number</button>
       </div>
     </div>
   );
---
+++ Projects/my-projects/src/proje2/app2.js
@@ -0,0 +1,17 @@
+import Images from "./image";
+import React,{useState} from "react";
+import "../proje2/proje2.css";
+
+
+export const App2 =()=>{
+    return(
+        <div className="App">
+            <div className="container">
+                <img src="" alt="selected" className="selected" />
+            </div>
+            <div className="imgContainer">
+                <img src="" alt="tantuni" />
+            </div>
+        </div>
+    );
+}
