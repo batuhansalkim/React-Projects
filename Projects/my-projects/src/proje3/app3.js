808536195c6dcce5411bf71ad5c13bb137101f33
Baturpcu <batusalkim11@gmail.com>
2023-03-22 21:04:42 +0300

    update

--- Projects/my-projects/src/proje3/app3.js
+++ Projects/my-projects/src/proje3/app3.js
@@ -1,11 +1,18 @@
- import "../proje3/app3.css";
- import "../proje3/index.css";
+import { useState } from "react";
+import "../proje3/app3.css";
+import "../proje3/index.css";
+
  const App3 =()=>{
+    const [newItem,setNewItem] = useState("");
+    const addItem=()=>{ 
+        console.log(newItem);
+        setNewItem("");
+    }
     return(
         <div className="container">
             <h1>Todo List App</h1>
-            <input type="text" placeholder="add an item..." />
-            <button>Add</button>
+            <input type="text" placeholder="add an item..." value={newItem} onChange={e=> setNewItem(e.target.value)}/>
+            <button onClick={()=>addItem()}>Add</button>
             <ul>
                 <li>react çalış</li>
                 <li>js çalış</li>
