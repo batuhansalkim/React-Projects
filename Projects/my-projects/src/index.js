8df81385fc3164d2db56cdb8f91215bc48096ca2
Baturpcu <batusalkim11@gmail.com>
2023-03-23 04:09:39 +0300

    update

--- Projects/my-projects/src/index.js
+++ Projects/my-projects/src/index.js
@@ -7,11 +7,11 @@ import App3 from './proje3/app3';
 import reportWebVitals from './reportWebVitals';
 import { App4 } from './proje4/app4';
 import Appbatu from './proje5/app5';
-
+import App6 from './proje6/app6';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Appbatu/>
+    <App6/>
   </React.StrictMode>
 );
 
--- Projects/my-projects/src/proje6/app6.js
+++ Projects/my-projects/src/proje6/app6.js
@@ -6,10 +6,21 @@ function App6(){
     const [holidays,setHolidays] = useState(data);
     return(
         <main>
-            <section className="container">
+            <section className="container11">
                 <h3>National Holidays Dates</h3>
                 {
-                    
+                   holidays.map((holiday=>{
+                    return(
+                        <div className="holiday" key={holiday.id}>
+                            <img src={holiday.img} alt="" />
+                            <div>
+                                <p>{holiday.title}</p>
+                                <p>{holiday.date}</p>
+                            </div>
+                        
+                        </div>
+                    )
+                   })) 
                 }
                 <button>Clear All</button>
             </section>
