454ff95418bef2ef79792334ff9e5f96cf59e7ef
Baturpcu <batusalkim11@gmail.com>
2023-03-23 03:57:04 +0300

    update

--- Projects/my-projects/src/proje6/app6.js
+++ Projects/my-projects/src/proje6/app6.js
@@ -1,11 +1,19 @@
 import React,{useState} from "react";
+import data from "../proje6/data";
 import "../proje6/app6.css";
 
 function App6(){
+    const [holidays,setHolidays] = useState(data);
     return(
-        <div>
-
-        </div>
+        <main>
+            <section className="container">
+                <h3>National Holidays Dates</h3>
+                {
+                    
+                }
+                <button>Clear All</button>
+            </section>
+        </main>
     )
 }
 
---
+++ Projects/my-projects/src/proje6/data.js
@@ -0,0 +1,26 @@
+export default[
+    {
+        id:1,
+        title:"Ulusal Egemenlik ve Çocuk Bayramı",
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE0lbD6C5k5UGdrS7ldCamDGbtSYTyyc4mKf07TWt0tUQoac5djRNS0xnA00wT6Vh-abU&usqp=CAU",
+        date:"23 Nisan",
+    },
+    {
+        id:2,
+        title:"Zafer Bayramı",
+        img:"https://www.mdto.org.tr/sites/default/files/2021-08/30-agustos.jpg",
+        date:"30 Ağustos",
+    },
+    {
+        id:3,
+        title:"Atatürkü Anma, Gençlik ve Spor Bayramı",
+        img:"https://www.tkgm.gov.tr/sites/default/files/users/user180/19-may%C4%B1s-inst.jpg",
+        date: "19mayıs",
+    },
+    {
+        id:4,
+        title:"Cumhuriyet Bayramı",
+        img:"https://cdn.uludagbilisim.com/haber-duyuru/29_ekim_cumhuriyet_bayrami.webp",
+        date:"29 Ekim",
+    }
+]
