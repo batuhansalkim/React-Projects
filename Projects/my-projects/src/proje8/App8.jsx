eb18eef075d6744685c0c9786cbf736b9a023420
Baturpcu <batusalkim11@gmail.com>
2023-03-24 04:49:12 +0300

    update

--- Projects/my-projects/src/proje8/App8.jsx
+++ Projects/my-projects/src/proje8/App8.jsx
@@ -3,28 +3,106 @@ import "../proje8/app8.css";
 function App8(){
 
     const [showFinalResult, setFinalResult] = useState(false);
+    const [score,setScore] =useState(0);
+    const [currentQuestion,setCurrentQuestion] = useState(0);
+    const questions = [
+    {
+      text: "I see myself as a successful person.",
+      options: [
+        { id: 0, text: "never", isCorrect: false },
+        { id: 1, text: "rarely", isCorrect: false },
+        { id: 3, text: "often", isCorrect: true },
+        
+      ],
+    },
+    {
+      text: "I can control my excitement around other people.",
+      options: [
+        { id: 0, text: "never", isCorrect: false },
+        { id: 1, text: "rarely", isCorrect: false },
+        { id: 3, text: "often", isCorrect: true },
+      ],
+    },
+    {
+      text: "I can deal with difficulties in life.",
+      options: [
+        { id: 0, text: "never", isCorrect: false },
+        { id: 1, text: "rarely", isCorrect: false },
+        { id: 3, text: "often", isCorrect: true },
+      ],
+    },
+    {
+      text: "I do not hesitate to participate in social events.",
+      options: [
+        { id: 0, text: "never", isCorrect: false },
+        { id: 1, text: "rarely", isCorrect: false },
+        { id: 3, text: "often", isCorrect: true },
+      ],
+    },
+    {
+      text: "I can express myself comfortably.",
+      options: [
+        { id: 0, text: "never", isCorrect: false },
+        { id: 1, text: "rarely", isCorrect: false },
+        { id: 3, text: "often", isCorrect: true },
+      ],
+    },
+    {
+      text: "I do not hesitate to take responsibility.",
+      options: [
+        { id: 0, text: "never", isCorrect: false },
+        { id: 1, text: "rarely", isCorrect: false },
+        { id: 3, text: "often", isCorrect: true },
+      ],
+    },
+    {
+      text: "I love myself.",
+      options: [
+        { id: 0, text: "never", isCorrect: false },
+        { id: 1, text: "rarely", isCorrect: false },
+        { id: 3, text: "often", isCorrect: true },
+      ],
+    },
+  ];
+  const optionClicked=(isCorrect)=>{
+    if(isCorrect){
+        setScore(score+1);
+    }if(currentQuestion +1 <questions.length){
+        setCurrentQuestion(currentQuestion+1);
+    }else{
+        setFinalResult(true);
+    }
+    
+  }
+  const restartTest=()=>{
+    setScore(0);
+    setFinalResult(false);
+    setCurrentQuestion(0);
+  }
     return(
         <div className="App">
             <div className="container">
                 <h1>Self Reliance App</h1>
-                <h2>Current Score : 2</h2>
+                <h2>Current Score : {score}</h2>
                 {
                     showFinalResult ? 
                     <div className="result">
                     <h1>Result</h1>
                     <h2>
-                        1 out of 7 self confident
+                        {score} out of {questions.length} you are  {(score/questions.length)*100}% self confident
                     </h2>
-                    <button>Restart Test</button>
+                    <button onClick={()=>restartTest()}>Restart Test</button>
                 </div>
                 :
                 <div className="question-card">
-                    <h2>Question 1 out of 7</h2>
-                    <h3>I see myself as a succesfull person</h3>
+                    <h2>Question {currentQuestion+1} out of {questions.length}</h2>
+                    <h3>{questions[currentQuestion].text}</h3>
                     <ul>
-                        <li>never</li>
-                        <li>rarely</li>
-                        <li>often</li>
+                        {questions[currentQuestion].options.map((option)=>{
+                           return(
+                            <li onClick={()=> optionClicked(option.isCorrect)} key={option.id}>{option.text}</li>
+                           ) 
+                        })}
                     </ul>
                 </div>
                 }
