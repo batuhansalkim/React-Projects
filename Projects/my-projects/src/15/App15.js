38eefabca1534a745bcc381815cdde7c22a96afd
Baturpcu <batusalkim11@gmail.com>
2023-03-25 22:51:20 +0300

    update

--- Projects/my-projects/src/15/App15.js
+++ Projects/my-projects/src/15/App15.js
@@ -1,7 +1,9 @@
+import { Navbar } from "./Navbar"
 export const App15=()=>{
     return(
         <div className="App">
             batu
+            <Navbar/>
         </div>
     )
 }
--- Projects/my-projects/src/15/Navbar.js
+++ Projects/my-projects/src/15/Navbar.js
@@ -1,7 +1,34 @@
+import React from "react";
+import { useState } from "react";
+import "../15/Navbar.css";
+
 export const Navbar =()=>{
+    const [burger_class,setBurgerClass] = useState("burger-bar unclicked");
+    const [menu_class,setMenuClass] = useState("menu hidden");
+    const [isMenuClicked, setIsMenuClicked] = useState(false);
+    function updateMenu(){
+        if (!isMenuClicked) {
+            setBurgerClass("burger-bar clicked");
+            setMenuClass("menu visible");
+        }else{
+            setBurgerClass("burger-bar unclicked");
+            setMenuClass("menu hidden");
+        }
+    }
     return(
-        <div>
-            Navbar
+        <div style={{width:"100%",height:"100vh"}}>
+            <nav>
+                <div className="burger-menu" onClick={updateMenu}>
+                    <div className={burger_class}></div>
+                    <div className={burger_class}></div>
+                    <div className={burger_class}></div>
+                </div>
+            </nav>
+            <div className={menu_class}>
+                <h3>Home</h3>
+                <h3>About</h3>
+                <h3>Contact</h3>
+            </div>
         </div>
     )
 }
