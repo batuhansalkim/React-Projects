e291314d9f45cb86f0cafd62cbcf813af8ed1088
Baturpcu <batusalkim11@gmail.com>
2023-03-25 04:13:13 +0300

    update

---
+++ Projects/my-projects/src/15/App15.js
@@ -0,0 +1,7 @@
+export const App15=()=>{
+    return(
+        <div>
+            batu
+        </div>
+    )
+}
--- Projects/my-projects/src/index.js
+++ Projects/my-projects/src/index.js
@@ -16,12 +16,14 @@ import { App10 } from './proje10/App10';
 import App11 from './11/App11';
 import App12 from './12/App12';
 import { App13 } from './13/App13';
-import App14 from './14/App14';
+// import App14 from './14/App14';
+import { App15 } from './15/App15';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App14 />
+    <App15 />
   </React.StrictMode>
 );
 
