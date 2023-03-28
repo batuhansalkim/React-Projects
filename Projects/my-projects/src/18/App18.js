a09f54a673e0b6379dfa1fdc1a15be9f9f7cb92b
Baturpcu <batusalkim11@gmail.com>
2023-03-28 16:28:17 +0300

    update

--- Projects/my-projects/src/18/App18.js
+++ Projects/my-projects/src/18/App18.js
@@ -3,36 +3,16 @@ import Users from "../18/users";
 import pagination from "../18/pagination";
 
 export function App18(){
-    const [users,setUsers]=useState();
+    const [pages,setPages] = useState([]);
     const [loading,setLoading] = useState(false);
-    const [page,setPage] = useState(1);
-    const [totalPages,setTotalPages] = useState(5);
 
-    useEffect(()=>{
-        const fetchusers = async()=>{
-            setLoading(true);
-            const res = await fetch("https://randomuser.me/api/?page=1&results=50&nat=us");
-            const data =await res.json();
-            console.log(data);
-            setLoading(false);
-            setUsers(data.results);
-        }
-        fetchusers();
-    },[])
-    const handleClick = num=>{
-        setPage(num);
-    }
+    
+    
+    
     return(
         <div>
         
-            <h1>Pagination App</h1>
-            <h5>Page : {page}</h5>
-            {
-                loading ? <p>Loading...</p>: <>
-                <Users users={users} page={page}/>
-                <pagination totalPages={totalPages} handleClick={handleClick}/>
-                </>
-            }
+            
         </div>
     )
 }
