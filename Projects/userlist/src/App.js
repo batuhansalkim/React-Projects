afc74af4ecb0e702d06ef62036899c69f7d8e3fe
Baturpcu <batusalkim11@gmail.com>
2023-03-31 01:46:52 +0300

    update

--- Projects/userlist/src/App.js
+++ Projects/userlist/src/App.js
@@ -1,20 +1,8 @@
-import './App.css';
-import { Info } from './components/info';
-import Form from './components/form';
-import { useState } from 'react';
-function App() {
-  // const [info,setInfo] = useState([]);
-  const [info,setInfo] = useState([]);
-  const [state,setState]  =useState(false);
-
+function App(){
   return(
-    <div className='App'>
-      {/* <Form info={info} setInfo={setInfo}/>
-      <Info info={info}  /> */}
-      <Form info={info} setInfo={setInfo} setState={setState}/>
-      <Info info={info} state={state}/>
+    <div>
+      
     </div>
   )
 }
-
 export default App;
