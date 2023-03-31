15543eaaf302f26e8a0eefb7ff848d17a11f828c
Baturpcu <batusalkim11@gmail.com>
2023-03-31 05:37:01 +0300

    update

--- Projects/userlist/src/App.js
+++ Projects/userlist/src/App.js
@@ -11,8 +11,13 @@ function App(){
     if(cart.indexOf(item)!==-1) return;
     setCart([...cart,item]);
   }
-  function handleChange(item,d){
+  const handleChange = (item, d) => {
+    const ind = cart.indexOf(item);
+    const arr = cart;
+    arr[ind].amount += d;
 
+    if (arr[ind].amount === 0) arr[ind].amount = 1;
+    setCart([...arr]);
   }
   return(
     <React.Fragment>
--- Projects/userlist/src/components/navbar.js
+++ Projects/userlist/src/components/navbar.js
@@ -1,14 +1,14 @@
 import React from "react";
 import {FaShoppingCart} from "react-icons/fa";
 import "../styles/navbar.css";
-const Navbar =({setShow})=>{
+const Navbar =({setShow,size})=>{
     return(
         <nav>
             <div className="nav_box">
                 <span className="my_shop" onClick={()=>setShow(true)}>My Shopping</span>
                 <div className="cart" onClick={()=>setShow(false)}>
                     <span className="shop-icon"><FaShoppingCart/></span>
-                    <span className="number">0</span>
+                    <span className="number">{size}</span>
                 </div>
             </div>
         </nav>
